f63c603d26e1b3bf63a6f7fa11fb6fe85ca9cb4e
Matthias Giger <i@matthiasgiger.com>
2024-03-02 18:54:21 +0100

    Add data labels with images

--- canvas.ts
+++ canvas.ts
@@ -1,8 +1,14 @@
-import { jsxFrameworks } from './data'
+import { jsxFrameworkLabels, jsxFrameworks } from './data'
 
 let context: CanvasRenderingContext2D
-const _data = jsxFrameworks()
-type Data = typeof _data
+const Space = {
+  small: 10,
+  medium: 20,
+  large: 40,
+}
+const data = jsxFrameworks()
+const labels = jsxFrameworkLabels
+type Data = typeof data
 const keys = [
   { label: 'react', color: 'red' },
   { label: 'preact', color: 'green' },
@@ -11,12 +17,13 @@ const keys = [
 type Keys = typeof keys
 const sizes = {
   padding: 50,
+  paddingRight: 100,
   width: 1000,
   height: 500,
-  chartWidth: 900,
+  chartWidth: 850,
   chartHeight: 400,
   xStart: 50,
-  xEnd: 950,
+  xEnd: 900,
   yStart: 450,
   yEnd: 50,
 }
@@ -42,16 +49,47 @@ function drawPoint(x: number, y: number, color = 'black') {
   context.fill()
 }
 
+function drawImage(url: string, x: number, y: number, width: number, height: number) {
+  const image = new Image()
+  image.src = url
+  image.onload = () => {
+    context.drawImage(image, x, y, width, height)
+  }
+}
+
+function drawLabel(text: string, image: string, x: number, y: number) {
+  context.beginPath()
+
+  const padding = Space.small
+  const imageSize = Space.medium
+  const textWidth = context.measureText(text).width
+  const boxWidth = textWidth + 2 * padding + imageSize + padding
+  const boxHeight = imageSize + 2 * padding
+
+  context.fillStyle = 'lightgray'
+  context.roundRect(x + padding, y - boxHeight / 2, boxWidth, boxHeight, Space.small)
+  context.fill()
+
+  context.fillStyle = 'black'
+  context.font = '24px Arial'
+  context.textAlign = 'left'
+  context.fillText(text, x + 3 * padding + imageSize, y + padding - 2)
+
+  drawImage(image, x + 2 * padding, y - padding, imageSize, imageSize)
+}
+
 export function renderLinesUntilIndex(maxIndex: number) {
-  const dataUntilIndex = _data.slice(0, maxIndex)
+  const dataUntilIndex = data.slice(0, maxIndex)
   const maximumRange = findMaximumRange(dataUntilIndex, keys)
 
-  for (const { label, color } of keys) {
+  for (const { label: labelKey, color } of keys) {
+    const label = labels[labelKey]
+    const labelPosition = { x: 0, y: 0 }
     context.beginPath()
     context.lineWidth = 2
     context.strokeStyle = color
     dataUntilIndex.forEach((value, index) => {
-      const current = value[label]
+      const current = value[labelKey]
 
       const positionX = index === 0 ? sizes.xStart : sizes.xStart + (sizes.chartWidth / (maxIndex - 1)) * index
       const positionY = sizes.yStart - (current / maximumRange) * sizes.chartHeight
@@ -60,8 +98,14 @@ export function renderLinesUntilIndex(maxIndex: number) {
       } else {
         context.moveTo(positionX, positionY)
       }
+
+      if (index === maxIndex - 1) {
+        labelPosition.x = positionX
+        labelPosition.y = positionY
+      }
     })
     context.stroke()
+    drawLabel(label.label, label.image, labelPosition.x, labelPosition.y)
   }
 }
 
@@ -120,9 +164,10 @@ export function intializeCanvas(canvas: HTMLCanvasElement) {
   canvas.height = height * 2
 
   sizes.padding = 50
+  sizes.paddingRight = 150
   sizes.width = canvas.width
   sizes.height = canvas.height
-  sizes.chartWidth = sizes.width - sizes.padding * 2
+  sizes.chartWidth = sizes.width - sizes.padding - sizes.paddingRight
   sizes.chartHeight = sizes.height - sizes.padding * 2
 
   sizes.xStart = sizes.padding
--- data.ts
+++ data.ts
@@ -1,3 +1,7 @@
+import preact from './asset/preact.png'
+import react from './asset/react.png'
+import solid from './asset/solid.png'
+
 const getRandomInteger = (minimum: number, maximum: number) => Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
 
 export function jsxFrameworks() {
@@ -15,4 +19,19 @@ export function jsxFrameworks() {
   }
 
   return data
+}
+
+export const jsxFrameworkLabels = {
+  react: {
+    label: 'React',
+    image: react,
+  },
+  preact: {
+    label: 'Preact',
+    image: preact,
+  },
+  solid: {
+    label: 'Solid',
+    image: solid,
+  },
 }
